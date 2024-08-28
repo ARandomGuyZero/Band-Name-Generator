6f02e005327d0643cd33a2fa93c000fcb67dda47
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-08-27 20:41:15 -0700

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,17 @@
+"""
+Band Name Generator
+
+This script generates a band name based on the user's inputs: the city they grew up in and the name of their pet.
+"""
+
+# Welcome message
+print("Welcome to the Band Name Generator")
+
+# Get the user's city
+city = input("What's the city you grew up in?\n")
+
+# Get the user's pet name
+pet = input("What's the name of your pet?\n")
+
+# Generate and display the band name
+print("Your band name is " + city + " " + pet)
